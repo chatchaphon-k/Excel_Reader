5ff845336b74601dc5c8b3194f824e934c39800f
Chatchaphon <ckitbuilder@gmail.com>
2021-07-22 12:04:19 +0700

    [Refactor] Remove unused codes

--- src/model/Reader_Excel.java
+++ src/model/Reader_Excel.java
@@ -21,49 +21,39 @@ public class Reader_Excel
     public Workbook Wb() { return wb; }
     public Sheet get_sheet() { return sheet; }
 
-    public Reader_Excel(String path2file, boolean is_xls) throws IOException
+    public Reader_Excel(String path2file) throws IOException
     {
-        set_workbook(path2file, is_xls);
+        set_workbook(path2file);
         set_firstSheet();
     }
 
-    public Reader_Excel(File file, boolean is_xls) throws IOException
+    public Reader_Excel(File file) throws IOException
     {
-        set_workbook(file, is_xls);
+        set_workbook(file);
         set_firstSheet();
     }
 
-    public Reader_Excel(String path2file, boolean is_xls, int sheet_num) throws IOException
+    public Reader_Excel(String path2file, int sheet_num) throws IOException
     {
-        set_workbook(path2file, is_xls);
+        set_workbook(path2file);
         set_sheet(sheet_num - 1);
     }
 
-    public Reader_Excel(File file, boolean is_xls, int sheet_num) throws IOException
+    public Reader_Excel(File file, int sheet_num) throws IOException
     {
-        set_workbook(file, is_xls);
+        set_workbook(file);
         set_sheet(sheet_num - 1);
     }
 
-    public void set_workbook(String path2file, boolean is_xls) throws IOException
+    public void set_workbook(String path2file) throws IOException
     {
-//        set_workbook(new FileInputStream(new File(path2file)), is_xls);
         set_workbook(new FileInputStream(new File(path2file)), path2file.substring(path2file.lastIndexOf(".") + 1));
     }
 
-    public void set_workbook(File file, boolean is_xls) throws IOException
+    public void set_workbook(File file) throws IOException
     {
-//        set_workbook(new FileInputStream(file), is_xls);
         String filename = file.getName();
         set_workbook(new FileInputStream(file), filename.substring(filename.lastIndexOf(".") + 1));
-    }
-
-    public void set_workbook(FileInputStream FIS, boolean is_xls) throws IOException
-    {
-        if(is_xls)
-            wb = new HSSFWorkbook(FIS);
-        else
-            wb = new XSSFWorkbook(FIS);
     }
 
     public void set_workbook(FileInputStream FIS, String ext) throws IOException
@@ -104,21 +94,8 @@ public class Reader_Excel
             while (cellIterator.hasNext())
             {
                 Cell cell = cellIterator.next();
-                //                    cell.setCellType(CellType.STRING);
                 DataFormatter dataFormatter = new DataFormatter();
                 System.out.print(dataFormatter.formatCellValue(cell) + "\t\t\t");
-//                    switch (cell.getCellType())
-//                    {
-//                        case STRING:    //field that represents string cell type  
-//                            System.out.print(cell.getStringCellValue() + "\t\t\t");
-//                            break;
-//                        case NUMERIC:    //field that represents number cell type  
-//                            System.out.print(cell.getNumericCellValue() + "\t\t\t");
-//                            break;
-//                        case _NONE:
-//                            System.out.print(cell.getDateCellValue()+ "\t\t\t");
-//                        default:
-//                    }
             }
             System.out.println("");
         }
@@ -137,14 +114,15 @@ public class Reader_Excel
         }
     }
 
-    public static void main(String[] args) {
-        try {
-            Reader_Excel imp_exl = new Reader_Excel("C:\\Users\\ckit\\Documents\\NetBeansProjects\\SubModules\\src\\052DS_AMS20190331.xls", true);
-            imp_exl.itr_rowCell_3();
-            imp_exl = new Reader_Excel("C:\\Users\\ckit\\Documents\\NetBeansProjects\\SubModules\\src\\Covid-19 Vaccination Record Survey (Relatives).xlsx", false);
-            imp_exl.itr_rowCell();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+//    public static void main(String[] args)
+//    {
+//        try {
+//            Reader_Excel imp_exl = new Reader_Excel("C:\\Users\\ckit\\Documents\\NetBeansProjects\\SubModules\\Excel\\Excel_Reader\\src\\052DS_AMS20190331.xls");
+//            imp_exl.itr_rowCell_3();
+//            imp_exl = new Reader_Excel("C:\\Users\\ckit\\Documents\\NetBeansProjects\\SubModules\\Excel\\Excel_Reader\\src\\Covid-19 Vaccination Record Survey (Relatives).xlsx");
+//            imp_exl.itr_rowCell();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
